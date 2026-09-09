f9cb68f48f90c51c5d8ab7dc0107fd98dcf18c16
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-09 20:20:52 +0400

    Update homework-16-backend-storage-interface.cpp

--- 3-POLYMORPHISM/homework/homework-16-backend-storage-interface.cpp
+++ 3-POLYMORPHISM/homework/homework-16-backend-storage-interface.cpp
@@ -1,72 +1,53 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
+#include<string>
 using namespace std;
 
-/*
-    Polymorphism
-
-    15. Abstract Classes as Interfaces
-*/
-
-/*
-    Exercise: Backend Data Storage Interface
-
-    Instructions:
-
-    1. Define an abstract interface named DataStorage with the following pure virtual functions:
-        void storeData(const string& data): This function should represent storing data.
-        string retrieveData(): This function should represent retrieving data.
-        Implement two concrete classes that inherit from the DataStorage interface:
-
-    2. FileStorage: Implement the functions to store and retrieve data using a file.
-        DatabaseStorage: Implement the functions to store and retrieve data using a database.
-        In the main function:
-
-    3. Create an instance of FileStorage, store and retrieve some data.
-    4. Create an instance of DatabaseStorage, store and retrieve some data.
-*/
-
-
-// Abstract interface for data storage
 class DataStorage {
-    // TODO: Declare pure virtual functions for storing and retrieving data
-    virtual ~DataStorage() {}   // Virtual destructor
+    public:
+    virtual ~DataStorage() {}  
+    virtual void storeData(string data) = 0;
+    virtual string retrieveData() = 0;
 };
 
-
-// Concrete class implementing data storage using a file
 class FileStorage : public DataStorage {
-    // TODO: Implement functions to store and retrieve data using a file
-
-    // Virtual destructor
+    private:
+    string fileData;
+    
+    public:
+    void storeData(string data) override {
+        fileData = data;
+        cout << "using file: " << data << endl;
+    }
+    string retrieveData() override {
+        return fileData;
+    }
     ~FileStorage() {
         cout << "FileStorage Destructor" << endl;
     }
 };
 
-
-// Concrete class implementing data storage using a database
 class DatabaseStorage : public DataStorage {
-    // TODO: Implement functions to store and retrieve data using a database
-
-    // Virtual destructor
+    private:
+    string dbData;
+    
+    public:
+    void storeData(string data) override {
+        dbData = data;
+        cout << "using database: " << data << endl;
+    }
+    string retrieveData() override {
+        return dbData;
+    }
     ~DatabaseStorage() {
         cout << "DatabaseStorage Destructor" << endl;
     }
 };
 
-
 int main() {
-    
-    // Example usage of the DataStorage interface
-
-    // Using FileStorage
     FileStorage fileStorage;
     fileStorage.storeData("FileStorage Data");
     cout << "FileStorage: " << fileStorage.retrieveData() << endl;
 
-    // Using DatabaseStorage
     DatabaseStorage databaseStorage;
     databaseStorage.storeData("DatabaseStorage Data");
     cout << "DatabaseStorage: " << databaseStorage.retrieveData() << endl;
