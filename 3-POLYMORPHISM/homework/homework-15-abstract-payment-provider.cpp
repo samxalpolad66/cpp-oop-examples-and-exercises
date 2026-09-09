e1148ad809d25260a04296d51dbc1fdee75cb1c5
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-09 19:53:56 +0400

    Update homework-15-abstract-payment-provider.cpp

--- 3-POLYMORPHISM/homework/homework-15-abstract-payment-provider.cpp
+++ 3-POLYMORPHISM/homework/homework-15-abstract-payment-provider.cpp
@@ -1,72 +1,36 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
 using namespace std;
-
-/*
-    Polymorphism
-
-    14. Pure virtual functions and Abstract classes
-*/
-
-
-/*
-    Problem Statement:
-    You are tasked with designing a system that can process payments using different payment methods in a web backend. 
-    Create a C++ program that demonstrates the use of an abstract class to model different types of payment providers.
-
-    Requirements:
-
-    1. Create an abstract class PaymentMethod with the following features:
-    Payment Processing Method: Include a method processPayment with a double parameter representing the amount to be processed.
-
-    2. Implement two concrete subclasses of PaymentMethod:
-    CreditCardPayment: Implement the processPayment function to display a message indicating that the payment is being processed using a credit card.
-    PayPalPayment: Implement the processPayment function to display a message indicating that the payment is being processed using PayPal.
-
-    3. Extend the PaymentMethod abstract class by adding the following common functionality:
-    Payment Attempt Logging: Implement a method logPaymentAttempt that takes a double parameter representing the payment amount and displays a message indicating that a payment attempt for the given amount has been logged.
-
-    4. In the main function, create instances of both CreditCardPayment and PayPalPayment. 
-    Use these instances to demonstrate:
-    Logging payment attempts for different amounts.
-    Processing payments using each payment method.
-
-    Note:
-    Consider which methods should be pure virtual functions based on the commonality and variability among payment methods.
-    Think about the common functionality that can be shared among different payment methods.
-*/
-
-
-
-// Abstract class representing a payment method
-class PaymentMethod {
+class PaymentMethod{
     public:
-        // TODO: Add a pure virtual function to process payment
+    virtual void ProcessPayment(double payment) = 0;
+    virtual ~PaymentMethod(){}
+    void logPaymentAttempt(double payment){
+        cout << "Payment attempt logged for amount: " << payment << endl;
+    }
 
-        // TODO: Add a common function to log payment attempts
 };
-
-// Concrete subclass for processing credit card payments
-class CreditCardPayment : public PaymentMethod {
+class CreditCardPayment : public PaymentMethod{
     public:
-        // TODO: Implement the processPayment function
-};
+    void ProcessPayment(double amount)override{
+        cout << "Payment with credit card: " << amount << endl;
+    }
 
-// Concrete subclass for processing PayPal payments
-class PayPalPayment : public PaymentMethod {
+};
+class PayPalPayment : public PaymentMethod{
     public:
-        // TODO: Implement the processPayment function
+    void ProcessPayment(double amount)override{
+        cout << "Payment with paypal: " << amount << endl;
+    }
+
 };
+int main(){
+    CreditCardPayment credit1;
+    PayPalPayment paypal1;
+    credit1.logPaymentAttempt(100);
+    credit1.ProcessPayment(100);
+    cout << "------------" <<endl;
+    paypal1.logPaymentAttempt(100);
+    paypal1.ProcessPayment(100);
 
 
-int main() {
-
-    // TODO: Create instances of CreditCardPayment and PayPalPayment
-
-    // TODO: Log payment attempts for different amounts
-
-    // TODO: Process payments using each payment method
-
-    return 0;
 }
