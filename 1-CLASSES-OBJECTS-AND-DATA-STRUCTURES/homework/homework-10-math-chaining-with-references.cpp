69c5c53ca77feee04c66352eb5805953a802bebd
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-09 14:43:51 +0400

    Update homework-10-math-chaining-with-references.cpp

--- 1-CLASSES-OBJECTS-AND-DATA-STRUCTURES/homework/homework-10-math-chaining-with-references.cpp
+++ 1-CLASSES-OBJECTS-AND-DATA-STRUCTURES/homework/homework-10-math-chaining-with-references.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
-
 using namespace std;
-
-/*
-    Exercise:
-    Implement a class called MathOperations that performs basic mathematical operations. 
-    The class should support chaining calls using references. 
-    The following operations should be supported:
-
-    1. MathOperations& add(int val): Adds the given value to the current result.
-    2. MathOperations& subtract(int val): Subtracts the given value from the current result.
-    3. MathOperations& multiply(int val): Multiplies the current result by the given value.
-    4. MathOperations& divide(int val): Divides the current result by the given value.
-    
-    Additionally, the class should provide a getResult() function that returns the current result.
-
-    In the main() function, create an instance of MathOperations and demonstrate 
-    the use of chained calls by performing the following operations:
-
-    1. Start with an initial result of 10.
-    2. Add 5, subtract 2, multiply by 3, and divide by 4.
-    3. Print the final result.
-    
-    Note: Ensure that the class handles division by zero gracefully and returns an appropriate message.
-*/
-
-
-/* Solution */
-
-
-
-
+class MathOperations{
+    private:
+    int result;
+    public:
+    MathOperations(int result){
+        this -> result = result;
+    }
+    MathOperations& add(int val){
+        result = result + val;
+        return *this;
+    }
+    MathOperations& subtract(int val){
+        result = result - val;
+        return *this;
+    }
+    MathOperations& multiply(int val){
+        result = result * val;
+        return *this;
+    }
+    MathOperations& divide(int val){
+        if(val != 0){
+            result = result / val;
+        }
+        else{
+            cout << "Invalid" <<endl;
+        }
+        return *this;
+    }
+    int getResult(){
+        return result;
+    }
+};
 int main() {
+    system("cls");
 
     /*      Example usage:     */
     MathOperations math(10);
