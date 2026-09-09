4fad486c10126a4ccfee2507e46441a4cd2baf07
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-09 15:05:04 +0400

    Update homework-08-static-keyword-stock-tracker.cpp

--- 3-POLYMORPHISM/homework/homework-08-static-keyword-stock-tracker.cpp
+++ 3-POLYMORPHISM/homework/homework-08-static-keyword-stock-tracker.cpp
@@ -1,148 +1,121 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
 using namespace std;
+/*double trackStockPrice(string symbol , double currentPrice){
+    static map<string,double>HighestPrice;
+    if(currentPrice > HighestPrice[symbol]){
+        HighestPrice[symbol] = currentPrice;
+    }
+    cout << "The highest price of stock name is: " << symbol << " and its highest price is: ";
+    return HighestPrice[symbol];
 
-
-/*
-    Static Keyword in C++
-*/
-
-
-/*
-    Exercise-1: Stock Price Tracker
-
-    Write a C++ program that simulates a stock price tracker function. 
-    The stock price tracker function should have the following properties:
-
-    1. Create a function named trackStockPrice that takes the stock symbol (a string) and the current stock price (a double) as parameters.
-    2. Inside the function, maintain a static local variable that stores the highest stock price observed for a given stock symbol.
-    3. Update the highest stock price if the current price is higher.
-    4. The function should return the highest stock price observed for the given stock symbol.
-
-
-*/
-double trackStockPrice(string symbol, double currentPrice) {
-    // your code
-}
-
-
-/*
-    Exercise-2: Bank Account Management
-
-    Create a C++ program that models a simplified bank account management system. 
-    In this system, you will create a BankAccount class with the following features:
-
-    1. Each BankAccount object should have a unique account number that starts from 1001 and increments by 1 for each new account created.
-    2. Each account should have an account balance.
-    3. Implement methods to deposit and withdraw funds from the account.
-    4. Implement a method to display the account details, including the account number and balance.
-*/
-class BankAccount {
-    // your code
 };
 
+int main(){
+    system("cls");
+    cout << trackStockPrice("Etherium" , 800) <<endl;
+    cout << trackStockPrice("Bitcoin" , 900) << endl;
+    cout << trackStockPrice("Etherium" , 400) << endl;
+}*/
+
+/*class Account{
+    private:
+    int AccountNumber;
+    double balance;
+    static int nextAccountNumber;
+    public:
+    Account(double balance){
+        this -> balance = balance;
+        AccountNumber = nextAccountNumber;
+        nextAccountNumber++;
+    }
+    ~Account(){
+        nextAccountNumber--;
+    }
+    void withdraw(double amount){
+        if(balance > amount && amount > 0){
+            balance = balance - amount;
+        }
+        else{
+            cout << "Invalid amount" <<endl;
+        }
+    }
+    void deposit(double amount){
+        if(amount > 0){
+            balance = balance + amount;
+        }
+        else{
+            cout << "Invalid amount" <<endl;
+        }
+    }
+    void displayInfo(){
+        cout << "Account Number: " << AccountNumber << "balance: " << balance << endl; 
+    }
+
+};
+int Account::nextAccountNumber = 1001;
+int main(){
+    Account acc1(500);
+    Account acc2(800);
+    acc1.withdraw(399);
+    acc2.deposit(800);
+    acc1.displayInfo();
+    acc2.displayInfo();
+}*/
 
 
-/*
-    Exercise-3: One instance Class
+/*class OnlyOneInstance {
+    private:
+        static OnlyOneInstance *instance;
+        OnlyOneInstance() {}
 
-    Create a C++ program that implements a  class, 
-    which ensures that only one instance(object) of the class can be created. 
-*/
-class OnlyOneInstance {
-    // your code
+    public:
+        OnlyOneInstance(const OnlyOneInstance &obj) = delete;
+        
+        static OnlyOneInstance *getInstance() {
+            if (instance == nullptr) {
+                instance = new OnlyOneInstance();
+                return instance;
+            } else {
+                return instance;
+            }
+        }
 };
 
+OnlyOneInstance *OnlyOneInstance::instance = nullptr;
+int main(){
+    OnlyOneInstance* obj1 = OnlyOneInstance::getInstance();
+    OnlyOneInstance* obj2 = OnlyOneInstance::getInstance();
+
+    if(obj1 == obj2){
+        cout << "Her ikisi eyni obyekte ishare edir!" << endl;
+    }
+}*/
 
 
-
-/*
-    Exercise-4: Prime Number Caching
-
-    Objective:
-    Write a C++ program that efficiently generates and caches prime numbers up to 100 and allows you to query the cached prime numbers.
-
-    Requirements:
-    1. Implement a function that generates prime numbers up to 100(e.g., generatePrimes(int limit)).
-    2. Cache the generated prime numbers for quick retrieval.
-    4. Create a menu-driven user interface to interact with the caching system.
-    5. Generate and cache prime numbers up to a specified limit.
-    6. Check if a number is prime using the cached data.
-*/
-static int primes[100] = {0};   // Static cache for Prime numbers
-
-void generateAndCachePrimes(int limit) {
-    // your code
+/*vector<int>GetPrimes(int number){
+    static vector<int>primes;
+    for(int num = 2 ; num <= number ; num++){
+        bool IsPrime = true;
+        for(int i = 2 ; i<num ; i++){
+            if(num%i == 0){
+                IsPrime = false;
+                break;
+            }
+        }
+        if(IsPrime){
+                primes.push_back(num);
+            }
+    }
+    return primes;
 }
+int main(){
+    system("cls");
+    vector<int>primes = GetPrimes(100);
+    for(int x : primes){
+        cout << x << " ";
+    }
+}*/
 
-
-
-
-
-int main() {
-    BankAccount account1;
-    account1.deposit(1000);
-    account1.withdraw(500);
-    account1.displayAccountDetails();
-
-    BankAccount account2;
-    account2.deposit(1500);
-    account2.displayAccountDetails();
-
-    // Display the total number of accounts created.
-    std::cout << "Total Accounts Created: " << BankAccount::totalAccounts << std::endl;
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-int main() {
-
-    //  Exercise-1: example usage
-    double price1 = trackStockPrice("AAPL", 150.25);
-    cout << "Highest AAPL Stock Price: $" << price1 << endl;
-
-    double price2 = trackStockPrice("GOOGL", 2700.50);
-    cout << "Highest GOOGL Stock Price: $" << price2 << endl;
-
-    double price3 = trackStockPrice("AAPL", 155.75);
-    cout << "Highest AAPL Stock Price: $" << price3 << endl;
-
-    double price4 = trackStockPrice("TSLA", 800.00);
-    cout << "Highest TSLA Stock Price: $" << price4 << endl;
-
-
-
-    //  Exercise-2: example usage
-    BankAccount account1;
-    account1.deposit(1000);
-    account1.withdraw(500);
-    account1.displayAccountDetails();
-
-    BankAccount account2;
-    account2.deposit(1500);
-    account2.displayAccountDetails();
-
-    // Display the total number of accounts created.
-    cout << "Total Accounts Created: " << BankAccount::totalAccounts << endl;
-
-
-
-    // Exercise-3: example usage
-    // your code ;-)
-
-
-
-    // Exercise-4: example usage
-    // your code ;-)
-
-
-}
