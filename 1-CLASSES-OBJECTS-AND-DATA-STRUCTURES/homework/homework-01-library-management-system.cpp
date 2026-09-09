48af70474b726b82cf3bbdd1f6148c74174adb23
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-09 14:08:51 +0400

    1: homework 01 - library management system

--- 1-CLASSES-OBJECTS-AND-DATA-STRUCTURES/homework/homework-01-library-management-system.cpp
+++ 1-CLASSES-OBJECTS-AND-DATA-STRUCTURES/homework/homework-01-library-management-system.cpp
@@ -1,44 +1,51 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 /*
     Exercise: Library Management System
-
     Description:
     Design and implement a Library Management System using classes and access specifiers in C++. 
     The system should allow you to manage books in a library.
-
     Requirements:
-
     1. Create a class named "Book" with the following attributes:
         Title (a string)
         Author (a string)
         Publication Year (an integer)
         ISBN (a string)
-       
     2. Make the attributes private to encapsulate them.
-
     3. Include member functions to:
         Set the book details (title, author, publication year, ISBN).
         Display the book details.
 
     4. Create objects of the "Book" class and demonstrate accessing and modifying the attributes using member functions.
-
     Tips:
     Use access specifiers (public and private) to control access to class members.
     You can choose to implement the member functions directly in the class declaration (inline) or define them outside the class.
     Test the functionality of the class by creating book objects, setting their details, and displaying the information.
     This exercise will help you practice using access specifiers to control the visibility of class members and understand the concept of encapsulation.
 */
-
-
-
 /*  Solution */
+class Book{
+    private:
+    string Title;
+    string Author;
+    int PublicYear;
+    string ISBN;
+    public:
+    void setBookDetails(string bookname  ,string author , int year , string isbn){
+       Title = bookname;
+       Author = author;
+       PublicYear = year;
+       ISBN = isbn;
+    }
+    void displayBookDetails(){
+        cout << Title <<endl;
+        cout << Author << endl;
+        cout << PublicYear << endl;
+        cout << ISBN << endl;
+    }
 
-
-
+};
 int main() {
 
     /*      Example usage:     */
