1e8927dbe6f71f0719383def518f8b7b4534bdfa
samxalpolad66 <samxalpolad666@gmail.com>
2026-09-09 14:19:06 +0400

    Update homework-04-employee-setters-and-getters.cpp

--- 1-CLASSES-OBJECTS-AND-DATA-STRUCTURES/homework/homework-04-employee-setters-and-getters.cpp
+++ 1-CLASSES-OBJECTS-AND-DATA-STRUCTURES/homework/homework-04-employee-setters-and-getters.cpp
@@ -1,41 +1,7 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
-/*
-    Exercise: Employee Management System
-
-    You are tasked with creating an employee management system for a company. 
-    Each employee has a name, age, position, and salary. 
-    Your goal is to implement a class called Employee that encapsulates 
-    these attributes and provides appropriate setters and getters.
-
-    Your task is to create a C++ class called Employee with the following specifications:
-
-    1. Private member variables:
-        name (string): Holds the name of the employee.
-        age (int): Holds the age of the employee.
-        position (string): Holds the position/title of the employee.
-        salary (double): Holds the salary of the employee.
-    
-    2. Public member functions:
-        setName(const string& name): Sets the name of the employee.
-        setAge(int age): Sets the age of the employee.
-        setPosition(const string& position): Sets the position of the employee.
-        setSalary(double salary): Sets the salary of the employee.
-        getName() const: Returns the name of the employee.
-        getAge() const: Returns the age of the employee.
-        getPosition() const: Returns the position of the employee.
-        getSalary() const: Returns the salary of the employee.
-    
-    Your implementation should allow external code to set and get the attributes of
-    an Employee object using the appropriate setter and getter functions.
-
-    Here's a code template to get you started:
-*/
-
-class Employee {
+class Employee{
     private:
         string name;
         int age;
@@ -43,15 +9,38 @@ class Employee {
         double salary;
 
     public:
-        // TODO: Implement setters and getters
+    void setName(const string& name){this -> name = name;}
+    void setAge(int age){this -> age = age;}
+    void setPosition(const string& position){this -> position = position;}
+    void setSalary(double salary){this -> salary = salary;}
 
+    string getName()const{return name;}
+    int getAge()const{return age;}
+    string getPosition() const{return position;}
+    double getSalary() const{return salary;}
 };
+int main(){
+    system("cls");
+    Employee employee;
+    employee.setName("Shamkhal");
+    employee.setAge(18);
+    employee.setPosition("data scientist");
+    employee.setSalary(0);
+    cout << "about of employee" << endl;
+    cout << employee.getName() << endl;
+    cout << employee.getAge() << endl;
+    cout << employee.getPosition() <<endl;
+    cout << employee.getSalary() << endl;
+    cout << "about of employee 2" << endl;
+    Employee employee2;
+    employee2.setName("Ali");
+    employee2.setAge(134);
+    employee2.setPosition("Angel");
+    employee2.setSalary(0);
+    cout << employee2.getName() << endl;
+    cout << employee2.getAge() << endl;
+    cout << employee2.getPosition() <<endl;
+    cout << employee2.getSalary() << endl;
 
 
-int main() {
-
-    // TODO: Test the Employee class by creating an object, setting attributes, and getting attribute values
-
-    return 0;
 }
-
